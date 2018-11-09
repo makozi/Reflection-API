e476a0eaf17726efa517f2b428e281f6bdb0546a
makozi <marizumakozi97@gmail.com>
2018-11-09 23:16:52 +0100

    update

---
+++ server.js
@@ -0,0 +1,30 @@
+//server.js
+  // "test": "echo \"Error: no test specified\" && exit 1 mocha",
+import express from 'express';
+
+
+
+
+const app= express()
+
+app.use(express.json())
+app.get('/', (req,res) =>{
+
+
+    return res.status(200).send({'message': 'YAY! Congratulations! Your first endpoint is working'});
+
+})
+
+
+
+import Reflection from './src/controllers/Reflection';
+app.post('/api/v1/reflections', Reflection.create);
+app.get('/api/v1/reflections', Reflection.getAll);
+app.get('/api/v1/reflections/:id', Reflection.getOne);
+app.put('/api/v1/reflections/:id', Reflection.update);
+app.delete('/api/v1/reflections/:id', Reflection.delete);
+
+app.listen(3000)
+console.log('app running on port ', 3000)
+
+
